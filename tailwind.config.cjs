e3e75c092639c46b2163efe582b0467c49bb514c
Sadiq <seeker99157@gmial.com>
2022-09-11 10:53:04 +0100

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        Orange: "hsl(26, 100%, 55%)",
+        Paleorange: "hsl(25, 100%, 94%)",
+        Verydarkblue: "hsl(220, 13%, 13%)",
+        Darkgrayishblue: "hsl(219, 9%, 45%)",
+        Grayishblue: "hsl(220, 14%, 75%)",
+        Lightgrayishblue: "hsl(223, 64%, 98%)"
+      }
+    },
+  },
+  plugins: [],
+}
